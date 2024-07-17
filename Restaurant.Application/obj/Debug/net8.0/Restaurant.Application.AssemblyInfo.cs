7a95bbe9f53dc0bb53d49b3bd2831680be5002c9
MohamadSadem0 <mohamad1.sadem@gmail.com>
2024-07-17 10:52:49 +0300

    .

--- Restaurant.Application/obj/Debug/net8.0/Restaurant.Application.AssemblyInfo.cs
+++ Restaurant.Application/obj/Debug/net8.0/Restaurant.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Restaurant.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92c0bfff9577f0440e27041ec00c1d641d9f53ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d6034f79b7ebeed57cb7e3956c6afa8c045dfac")]
 [assembly: System.Reflection.AssemblyProductAttribute("Restaurant.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Restaurant.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
